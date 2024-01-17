1a273e50808ee9e5a1478585dd09faef7a535414
Mjnorms <mjnorms@gmail.com>
2024-01-17 10:21:17 -0800

    feat(main): add checkAndResetIfTomorrow function
    Added a new function `checkAndResetIfTomorrow` to `main.py` that checks if the current date is different from the last recorded timestamp in the `current_servings.json` file. If the dates don't match or if there are missing or improperly formatted data, the function calls `collectServingsFromDefault` to reset the servings. Otherwise, it retrieves the saved servings from the file.
    
    This addition ensures that the servings are correctly reset based on the current date and preserves the previously saved servings when the dates match.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import json
+from datetime import datetime
+from datetime import timedelta
 
 # current servings per day
 current_servings = {
     "vegetables": -1,
     "fruits": -1,
     "grains": -1,
-    "dairy": 1,
+    "dairy": -1,
     "protein": -1
 }
 
@@ -13,7 +15,7 @@ current_servings = {
 def main():
     print("Welcome to Melon")
     printServingsLeft()
-    collectServingsFromDefault()
+    checkAndResetIfTomorrow()
     printServingsLeft()
     storeServingsToJSON()
 #//////////////////////////////////////////////////////////////
@@ -35,9 +37,37 @@ def collectServingsFromDefault():
 
 # Store the current servings left to JSON
 def storeServingsToJSON():
+    local_data = {
+        "timestamp": datetime.now().isoformat()
+    }
     with open("current_servings.json", "w") as json_file:
-        json.dump(current_servings, json_file)
+        json.dump({"local_data": local_data, "current_servings": current_servings}, json_file)
 
+def checkAndResetIfTomorrow():
+    global current_servings
+    for serving in current_servings:
+        count = current_servings[serving]
+        if count == -1:
+            collectServingsFromDefault()
+            return
+
+    try:
+        with open("current_servings.json", "r") as json_file:
+            data = json.load(json_file)
+            saved_local_data= data.get("local_data", {})
+            saved_servings = data.get("current_servings", {})
+
+    except (FileNotFoundError, json.decoder.JSONDecodeError, KeyError):
+        # Handle the case where the file is not found, empty, or not properly formatted
+        collectServingsFromDefault()
+        return
+    
+    current_datetime = datetime.now()
+    saved_timestamp = saved_local_data.get("timestamp")
+    if saved_timestamp and current_datetime.date() > datetime.fromisoformat(saved_timestamp).date():
+        collectServingsFromDefault()
+    else:
+        current_servings = saved_servings
 
 if __name__ == "__main__":
     main()
